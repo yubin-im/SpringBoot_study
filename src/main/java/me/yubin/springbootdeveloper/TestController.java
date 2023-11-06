10e98348966266eb8be76f0ddc98893180a9d009
[yubin-im] <[ybyb130@gmail.com]>
2023-11-07 01:17:54 +0900

    first-commit

---
+++ src/main/java/me/yubin/springbootdeveloper/TestController.java
@@ -0,0 +1,14 @@
+package me.yubin.springbootdeveloper;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World!";
+    }
+
+}
