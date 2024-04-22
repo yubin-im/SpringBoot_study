fd6d64612cf7e3f5b0991285436f458c9f030b89
yubin-im <ybyb130@gmail.com>
2024-04-22 17:23:13 +0900

    3-02 페이징 기능 추가

--- 점프투스프링부트/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ 점프투스프링부트/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -14,12 +14,16 @@ import org.springframework.transaction.annotation.Transactional;
 import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 
 @SpringBootTest
 class SbbApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
+
+	@Autowired
+	private QuestionService questionService;
 
 	@Transactional
 	@Test
@@ -32,5 +36,14 @@ class SbbApplicationTests {
 
 		assertEquals(1, answerList.size());
 		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
+
+	@Test
+	void testJpa2() {
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
 	}
 }
