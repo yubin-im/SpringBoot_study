98874eae2858f38ab97f1080e79dd3163cb6c15e
yubin-im <ybyb130@gmail.com>
2024-04-22 15:01:43 +0900

    로컬 삭제 파일 원격 레포지토리 반영

--- src/main/java/me/yubin/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package me.yubin.springbootdeveloper.repository;
-
-import me.yubin.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
